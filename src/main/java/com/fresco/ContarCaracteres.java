8451ca5035fe7ef72ea2a431aa7dcc2cf16a267c
jairo <jgrateron@gmail.com>
2024-03-23 12:07:51 -0400

    delete comparators

--- src/main/java/com/fresco/ContarCaracteres.java
+++ src/main/java/com/fresco/ContarCaracteres.java
@@ -7,31 +7,23 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class ContarCaracteres {
-    public static void main(String[] args) throws IOException {
-        var mapCaracteres = Files.lines(Path.of("./CalculateAverage_jgrateron.java"))
-                .map(s -> List.of(s.split("")))
-                .flatMap(List::stream)
-                .filter(c -> !c.isBlank())
-                .collect(Collectors.groupingBy(String::toString, Collectors.counting()));
+	public static void main(String[] args) throws IOException {
+		var mapCaracteres = Files.lines(Path.of("src/main/java/com/fresco/CalculateAverage_jgrateron.java"))
+				.map(s -> List.of(s.split(""))).flatMap(List::stream).filter(c -> !c.isBlank())
+				.collect(Collectors.groupingBy(String::toString, Collectors.counting()));
 
-        Comparator<Map.Entry<String, Long>> compararValue = Map.Entry.comparingByValue();
+		var i = new AtomicInteger(0);
 
-        Comparator<Map.Entry<String, Long>> compararKey = Map.Entry.comparingByKey();
-
-        var i = new AtomicInteger(0);
-
-        Consumer<Map.Entry<String, Long>> consumerEntry = e -> {
-            System.out.println("%02d) %s: %d".formatted(i.incrementAndGet(), e.getKey(), e.getValue()));
-        };
-
-        mapCaracteres.entrySet().stream()
-                .sorted(compararValue.reversed().thenComparing(compararKey))
-                .limit(20)
-                .forEach(consumerEntry);
-    }
+		System.out.println("-".repeat(500));
+		mapCaracteres.entrySet().stream()//
+				.sorted(Comparator.comparing(Map.Entry<String, Long>::getValue).reversed()
+						.thenComparing(Map.Entry.comparingByKey()))//
+				.limit(20)//
+				.forEach(e -> {
+					System.out.println("%02d) %s: %d".formatted(i.incrementAndGet(), e.getKey(), e.getValue()));
+				});
+	}
 }
-
