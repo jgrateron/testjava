d6ac503b1fd33cc5a8f3750d8232c26f633549b1
jairo <jgrateron@gmail.com>
2024-10-05 12:24:13 -0400

    upd cpu info

--- src/main/java/com/fresco/CpuInfo.java
+++ src/main/java/com/fresco/CpuInfo.java
@@ -9,8 +9,7 @@ import java.util.stream.Collectors;
 
 public class CpuInfo {
 
-	public static void main(String[] args) throws IOException {
-
+	public static void test1() throws IOException {
 		var mapCpuInfo = Files.lines(Path.of("/proc/cpuinfo"))//
 				.map(l -> {
 					var ar = l.split(":");
@@ -25,5 +24,28 @@ public class CpuInfo {
 					var join = String.join(", ", e.getValue());
 					System.out.println("%-20s: %s".formatted(e.getKey(), join));
 				});
+
+	}
+
+	public static void test2() throws IOException {
+		var mapCpuInfo = Files.lines(Path.of("/proc/cpuinfo"))//
+				.map(l -> {
+					var ar = l.split(":");
+					return ar.length == 2 ? Map.entry(ar[0].trim().toLowerCase(), ar[1].trim()) : null;
+				}).filter(Objects::nonNull)//
+				.collect(Collectors.groupingBy(Map.Entry::getKey,
+						Collectors.mapping(Map.Entry::getValue, Collectors.toList())));
+
+		mapCpuInfo.entrySet().stream()//
+				.sorted(Map.Entry.comparingByKey())//
+				.forEach(e -> {
+					var join = String.join(", ", e.getValue().stream().distinct().toList());
+					System.out.println("%-20s: %s".formatted(e.getKey(), join));
+				});
+	}
+
+	public static void main(String[] args) throws IOException {
+		test1();
+		test2();
 	}
 }
