bcf3993384ffb317e1dce6f0b8ccd515660c0ddd
jairo <jgrateron@gmail.com>
2024-10-17 11:15:43 -0400

    documentation

---
+++ src/main/java/com/fresco/Documentation.java
@@ -0,0 +1,23 @@
+package com.fresco;
+
+public class Documentation {
+
+	/**
+	 * sum of two integers
+	 * 
+	 * @param a the first value
+	 * @param b the second value
+	 * @return the result
+	 * @author jairo
+	 * @since 2024-10-14
+	 */
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		sum(1, 2);
+	}
+}
+
+
