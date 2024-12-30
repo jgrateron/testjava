ea70515ca5cca6cabdd49b1a1d96df9a8d3da4b3
jairo <jgrateron@gmail.com>
2024-12-30 16:45:43 -0400

    add record generics

--- src/main/java/com/fresco/RecordGeneric.java
+++ src/main/java/com/fresco/RecordGeneric.java
@@ -8,14 +8,21 @@ public class RecordGeneric {
 	public record Tuple<T, U>(T first, U second) {
 	}
 
-	public static void main(String[] args) {
-
-		var pair = new Pair<String>("Jairo", "Grateron");
-		System.out.println(pair);
-
-		var tuple = new Tuple<Integer, String>(45, "Jairo Grateron");
-		System.out.println(tuple);
-
+	public record Triple<T, U, W>(T first, U second, W third) {
 	}
 
+	public static void main(String[] args) {
+
+		var intPair = new Pair<Integer>(10, 20);
+		System.out.println(intPair);
+
+		var strPair = new Pair<String>("Hello", "World");
+		System.out.println(strPair);
+
+		var tuple = new Tuple<String, Integer>("Hello World", 30);
+		System.out.println(tuple);
+
+		var triple = new Triple<String, Integer, Double>("Hello World", 21, Math.PI);
+		System.out.println(triple);
+	}
 }
