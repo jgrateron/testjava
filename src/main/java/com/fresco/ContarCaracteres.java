9cc04532d1c01590d89a3ee5a1e58d948b844301
jairo <jgrateron@gmail.com>
2025-01-13 11:14:50 -0400

    se elimina el uso de atomicInt

--- src/main/java/com/fresco/ContarCaracteres.java
+++ src/main/java/com/fresco/ContarCaracteres.java
@@ -5,8 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ContarCaracteres {
 	public static void main(String[] args) throws IOException {
@@ -16,15 +16,16 @@ public class ContarCaracteres {
 				.map(ch -> String.valueOf((char) ch.intValue()))
 				.collect(Collectors.groupingBy(String::toString, Collectors.counting()));
 
-		var i = new AtomicInteger(0);
-
-		System.out.println("-".repeat(500));
-		mapCaracteres.entrySet().stream()//
+		var listaOrdenada = mapCaracteres.entrySet().stream()//
 				.sorted(Comparator.comparing(Map.Entry<String, Long>::getValue).reversed()
 						.thenComparing(Map.Entry.comparingByKey()))//
-				.limit(20)//
-				.forEach(e -> {
-					System.out.println("%02d) %s: %d".formatted(i.incrementAndGet(), e.getKey(), e.getValue()));
+				.limit(20)
+				.toList();
+
+		IntStream.range(0, listaOrdenada.size())
+				.forEach(i -> {
+					var e = listaOrdenada.get(i);
+					System.out.println("%02d) %s: %d".formatted(i, e.getKey(), e.getValue()));
 				});
 	}
 
