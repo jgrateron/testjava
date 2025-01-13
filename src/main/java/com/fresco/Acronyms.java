459556fe2dd8d35ab74e9750be0bc0d6726825a7
jairo <jgrateron@gmail.com>
2025-01-13 11:48:30 -0400

    upd acronyms

--- src/main/java/com/fresco/Acronyms.java
+++ src/main/java/com/fresco/Acronyms.java
@@ -1,19 +1,31 @@
 package com.fresco;
 
-import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class calendario {
+public class Acronyms {
 
 	public static void main(String[] args) {
-		var localDate = LocalDate.of(2024, 1, 1);
-		System.out.println(localDate);
 
-		var text = "National Aeronautics Space Administration";
-		var ar = text.split(" ");
-		var acro = Arrays.stream(ar).map(s -> s.substring(0, 1)).collect(Collectors.joining());
-		System.out.println(acro.toUpperCase());
+		var texts = """
+				Global Positioning System
+				National Aeronautics Space Administration
+				Hyper Text Markup Language
+				Uniform Resource Locator
+				Carbon copy
+				Laugh out loud
+				Chief Executive Officer
+				British Broadcasting Corporation
+				""";
+		texts.lines()
+				.forEach(text -> {
+					var acron = Stream.of(text.split(" "))
+							.filter(Predicate.not(String::isBlank))
+							.map(s -> s.substring(0, 1))
+							.collect(Collectors.joining())
+							.toUpperCase();
+					System.out.println("%-5s: %s".formatted(acron, text));
+				});
 	}
-
 }
