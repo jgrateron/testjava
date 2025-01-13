393a45cabd650067a0fdf67ce4c96fe9fccb3141
jairo <jgrateron@gmail.com>
2025-01-13 11:19:30 -0400

    up contar archivos directorios

--- src/main/java/com/fresco/ContarArchivosDirectorios3.java
+++ src/main/java/com/fresco/ContarArchivosDirectorios3.java
@@ -4,39 +4,51 @@ import static java.nio.file.Files.isDirectory;
 import static java.nio.file.Files.isSymbolicLink;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ContarArchivosDirectorios3 {
 
 	private record Acum(int nroArc, int nroDir, int total) {
+
+		public Acum combine(Acum o) {
+			return new Acum(nroArc + o.nroArc, nroDir + o.nroDir, total + o.total);
+		}
+
 		@Override
 		public final String toString() {
-			return "[%d %d %d]".formatted(nroArc, nroDir, total);
+			var strNroArc = String.valueOf(nroArc);
+			var strNroDir = String.valueOf(nroDir);
+			var strTotal = String.valueOf(total);
+			return "[%4s, %4s, %4s]".formatted(strNroArc, strNroDir, strTotal);
 		}
 	}
 
 	public static Acum contarArchivosDirectorio(Path path) throws IOException {
 		var result = Files.list(path)//
+				.sorted()
 				.peek(p -> {
 					if (isDirectory(p) && !isSymbolicLink(p)) {
 						try {
 							var inter = contarArchivosDirectorio(p);
-							System.out.println(p + " " + inter);
+							System.out.println(inter + " " + p);
+						} catch (AccessDeniedException e) {
+							// ignore
 						} catch (IOException e) {
-							System.out.println(e.getMessage());
+							throw new UncheckedIOException(e);
 						}
 					}
 				})//
 				.map(p -> new Acum(Files.isRegularFile(p) ? 1 : 0, isDirectory(p) ? 1 : 0, 1))//
-				.reduce(new Acum(0, 0, 0),
-						(a, n) -> new Acum(a.nroArc + n.nroArc, a.nroDir + n.nroDir, a.total + n.total));
+				.reduce(new Acum(0, 0, 0), Acum::combine);
 		return result;
 	}
 
 	public static void main(String[] args) throws IOException {
-		String path = "/etc/";
+		String path = "/var/log";
 		var result = contarArchivosDirectorio(Path.of(path));
-		System.out.println(path + " " + result);
+		System.out.println(result + " " + path);
 	}
 }
